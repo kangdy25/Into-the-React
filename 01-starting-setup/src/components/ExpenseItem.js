d71f97c84a50ea477b2b6962c974fb8321b088a3
Shineast <canfrog@naver.com>
2023-03-08 10:50:12 +0900

    Revert "Make styling in React"
    This reverts commit 31d68c51358b5120cb96787e728e49c35bece8bb.

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,5 @@
-import "./ExpenseItem.css";
-
 function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>2023.03.08</div>
-            <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
-            </div>
-        </div>
-    );
+    return <h2>Expense item!</h2>
 }
 
 export default ExpenseItem;
