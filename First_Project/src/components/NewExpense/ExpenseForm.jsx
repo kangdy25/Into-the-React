edd0c3d28194a059be9ed2747e0a99767b6dc75a
Shineast <canfrog@naver.com>
2023-03-16 17:40:10 +0900

    Learn Lifting State Up

--- First_Project/src/components/NewExpense/ExpenseForm.jsx
+++ First_Project/src/components/NewExpense/ExpenseForm.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -11,7 +11,6 @@ const ExpenseForm = () => {
     //     enteredAmount: '',
     //     enteredDate: '',
     // });
-
 
     const titleChangeHandler = (event) => {
         // 1번 방식
@@ -53,7 +52,7 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate),
         }
-        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
