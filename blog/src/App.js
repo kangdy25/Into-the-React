42c8bae32df624bd01ea0d97d126444011c01383
Shineast <canfrog@naver.com>
2023-04-07 00:02:54 +0900

    Learn state, useState

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,34 +2,38 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let postDate = "4월 6일에 씀";
-  const [글제목1, setA] = useState('남자 코트 추천');
-  const [글제목2, setB] = useState('강남 우동 맛집');
-  const [글제목3, setC] = useState('파이썬 독학');
-
+  const postDate = "4월 6일에 씀";
+  const [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [like, setLike] = useState(0);
 
   return (
     <div className="App">
       <div className='black-nav'>
         <h4>블로그임</h4>
       </div>
+      <div className='sort'>
+        <button className='sort__button' onClick={() => {
+          let sortContent = [...title];
+          sortContent.sort();
+          setTitle(sortContent);
+        }}>세끼... 정렬!!</button>
+      </div>
       <div className='list'>
-        <h4>{글제목1}</h4>
+        <h4>{title[0]}<span className='title' onClick={() =>{
+          let copyTitle = [...title];
+          copyTitle[0] = '여자 코트 추천';
+          setTitle(copyTitle);
+          }}>제목 바꾸기</span></h4>
         <p>{postDate}</p>
       </div>
       <div className='list'>
-        <h4>{글제목2}</h4>
+        <h4>{title[1]}<span onClick={() => {setLike(like + 1)}}>👍🏻 </span>{like}</h4>
         <p>{postDate}</p>
       </div>
       <div className='list'>
-        <h4>{글제목3}</h4>
+        <h4>{title[2]}<span onClick={() => {setLike(like + 1)}}>👍🏻 </span>{like}</h4>
         <p>{postDate}</p>
       </div>
-    
-    
-    
-    
-    
     </div>
   );
 }
