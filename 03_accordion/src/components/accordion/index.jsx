92b1576e11c4c648023ace15a953c5dc906da5f1
Kangdy <canfrog@naver.com>
2024-02-14 13:05:54 +0900

    make basic accordian structure

---
+++ 03_accordion/src/components/accordion/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import data from "./data";
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(0);
+    
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId)
+    }
+
+    return (<div className="wrapper">
+        <div className="accordian">
+            {
+                data && data.length > 0 ? 
+                data.map(a=>{
+                    return (
+                        <div className="item" key={a.id}>
+                            <div 
+                            onClick={()=>{handleSingleSelection(a.id)}} 
+                            className="title"
+                            >
+                                <h3>{a.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === a.id ?
+                                <div className="content">{a.answer}</div>
+                                : null
+                            }
+                        </div>
+                    )   
+                })
+                : <div>No Data Found!</div>
+            }
+        </div>
+
+    </div>)
+}
