420cc38e0cd11453a2bcbbea7ec504ad05be922e
Kangdy <canfrog@naver.com>
2024-07-18 23:50:39 +0900

    Finish 04 Project

--- 04_random_color_generator/src/App.js
+++ 04_random_color_generator/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function RandomColor() {
+    const [typeOfColor, setTypeOfColor] = useState('hex');
+    const [color, setColor] = useState('#000000');
+    
+    function randomColorUtility(len) {
+        return Math.floor(Math.random() * len);
+    }
+
+    function handleCreateHexRandomColor() {
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+        let hexColor = "#";
+
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+        setColor(hexColor)
+    }
+
+    function handleCreateRGBRandomColor() {
+        const r = randomColorUtility(256);
+        const g = randomColorUtility(256);
+        const b = randomColorUtility(256);
+        setColor(`rgb(${r}, ${g}, ${b})`)
+    }
+
+    useEffect(()=>{
+      if (typeOfColor === 'rgb') handleCreateRGBRandomColor()
+      else handleCreateHexRandomColor()
+    }, [typeOfColor])
+
+    return (
+        <div className="container"
+        style={{width: "100vw",
+          height: "100vh", 
+          background: color,
+          textAlign: "center",}}>
+            <button onClick={()=>{
+              setTypeOfColor('hex')
+            }}>Create HEX Color</button>
+            <button onClick={()=>{
+              setTypeOfColor('rgb')
+            }}>Create RGB Color</button>
+            <button onClick={typeOfColor === 'hex' ? handleCreateHexRandomColor : handleCreateRGBRandomColor}>Generate Random Color</button>
+            <div style={{
+                display : 'flex',
+                justifyContent : 'center',
+                alignItems : 'center',
+                color : '#fff',
+                fontSize : '60px',
+                marginTop : '50px',
+                flexDirection : 'column',
+            }}>
+                <h3>{typeOfColor === 'rgb' ? 'RGB Color' : 'HEX Color'}</h3>
+                
+                <h1>{color}</h1>
+            </div>
+        </div>
+    );
 }
 
-export default App;
