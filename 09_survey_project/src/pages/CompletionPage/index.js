3c8595721e0e6e83e448d79ebced223f7ee5c13c
Kangdy <canfrog@naver.com>
2025-01-28 16:49:48 +0900

    Axios 라이브러리 설치

--- 09_survey_project/src/pages/CompletionPage/index.js
+++ 09_survey_project/src/pages/CompletionPage/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const CompletionPage = () => {
+  axios.get('http://localhost:3001/surveys').then((res) => {
+    console.log('res', res.data);
+  });
+
   return <CompletionPageWrapper>CompletionPage</CompletionPageWrapper>;
 };
 
