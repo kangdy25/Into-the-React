b4dfa3a25ed29dc8d95bf83516f9a0a9b3fd38d0
Shineast <canfrog@naver.com>
2025-02-01 16:59:08 +0900

    설문조사 어드민 페이지 - useSWR API 연결

--- 09_survey_admin_project/src/pages/ListPage.jsx
+++ 09_survey_admin_project/src/pages/ListPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import useSWR from 'swr';
 import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
 
 const ListPage = () => {
+  const { data, error } = useSWR('/surveys', fetcher);
+
+  console.log('data: ', data);
+
+  if (error) {
+    return 'error';
+  }
   return <MainLayout>ListPage</MainLayout>;
 };
 
