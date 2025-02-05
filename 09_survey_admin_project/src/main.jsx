4d4832f25b6492c88be62c186fbd7b8f8efca9eb
Kangdy <canfrog@naver.com>
2025-02-05 12:45:00 +0900

    어드민 페이지 Redux로 전역 상태관리

--- 09_survey_admin_project/src/main.jsx
+++ 09_survey_admin_project/src/main.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import 'antd';
 import './index.css';
 import App from './App.jsx';
+import store from './stores/index.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
