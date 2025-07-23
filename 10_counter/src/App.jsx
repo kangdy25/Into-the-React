e088f70d95c15a71e268a72a48232eb7e08517ce
Shineast <canfrog@naver.com>
2025-07-23 16:19:09 +0900

    Control Lifecycle of Component in Counter App (2)

--- 10_counter/src/App.jsx
+++ 10_counter/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const isMount = useRef(false)
+
+  // 1. 마운트 : 탄생
   useEffect(()=>{
-    console.log(`count: ${count}`)
-  }, [count])
+    console.log("mount")
+  }, [])
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(()=>{
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update")
+  })
+
+  // 3. 언마운트 : 죽음
 
   const onClickButton = (value) => {
     setCount(count + value);
