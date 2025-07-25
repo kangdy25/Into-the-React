ba6918311e9075c60dbd9693744feebee0e3aab7
Shineast <canfrog@naver.com>
2025-07-25 11:33:28 +0900

    Setting mockdata

--- 11_todolist/src/App.jsx
+++ 11_todolist/src/App.jsx
@@ -4,7 +4,30 @@ import List from './components/List'
 import Editor from './components/Editor'
 import { useState } from 'react'
 
+const mockData = [
+    {
+      id: 0,
+      isDone: false,
+      content: "React 공부하기",  
+      date: new Date().getTime(),
+    },
+    {
+      id: 1,
+      isDone: false,
+      content: "성경 읽기",  
+      date: new Date().getTime(),
+    },
+    {
+      id: 2,
+      isDone: false,
+      content: "머슬업 훈련하기",  
+      date: new Date().getTime(),
+    },
+  ]
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  console.log(todos)
 
   return (
     <div className='App'>
