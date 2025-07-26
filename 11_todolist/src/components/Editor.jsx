a1b10adefd64261e7594753742e9199e0b2788b7
Shineast <canfrog@naver.com>
2025-07-26 11:44:32 +0900

    Implement create todo function

--- 11_todolist/src/components/Editor.jsx
+++ 11_todolist/src/components/Editor.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './Editor.css'
-const Editor = () => {
+const Editor = ({onCreate}) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  }
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    };
+    onCreate(content);
+    setContent("")
+  }
+
+  const onKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  }
+
   return (
     <div className='Editor'>
-      <input placeholder='새로운 TODO'/>
-      <button>추가</button>
+      <input 
+        ref={contentRef}
+        value={content} 
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent} 
+        placeholder='새로운 TODO'
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   )
 }
