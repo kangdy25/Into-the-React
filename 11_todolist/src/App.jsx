52ae62455c7cad65cd6d44efa9ef59008599d89d
Kangdy <canfrog@naver.com>
2025-07-28 11:32:37 +0900

    Replace useState to useReducer

--- 11_todolist/src/App.jsx
+++ 11_todolist/src/App.jsx
@@ -2,8 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import List from './components/List'
 import Editor from './components/Editor'
-import { useState, useRef } from 'react'
-import Reducer from './components/Reducer'
+import { useRef, useReducer } from 'react'
 
 const mockData = [
     {
@@ -24,40 +23,62 @@ const mockData = [
       content: "머슬업 훈련하기",  
       date: new Date().getTime(),
     },
-  ]
+]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": 
+      return [action.data, ...state]
+    case "UPDATE": 
+      return state.map((item) => 
+        item.id === action.targetId 
+          ? {...item, isDone: !item.isDone} 
+          : item 
+      ) 
+    case "DELETE":
+      return state.filter(
+        (item) => item.id !== action.targetId
+      )
+    default:
+      return state
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData)
   const idRef = useRef(3)
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,  
-      date: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos])
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,  
+        date: new Date().getTime(),
+      }
+    })
   }
 
   const onUpdate = (targetId) => {
-    setTodos(todos.map((todo) => 
-      todo.id === targetId 
-        ? {...todo, isDone: !todo.isDone}
-        : todo
-    ))
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId
+    })
   }
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId)) 
+    dispatch({
+      type: "DELETE",
+      targetId: targetId
+    })
   }
 
   return (
     <div className='App'>
-      <Reducer />
-      {/* <Header />
+      <Header />
       <Editor onCreate={onCreate}/>
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/> */}
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
     </div>
   )
 }
