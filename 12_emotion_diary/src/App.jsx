cea2dfd045853cfdbe3ad8d856cb2478aa9c15cf
Kangdy <canfrog@naver.com>
2025-08-01 10:14:44 +0900

    Page Navigation

--- 12_emotion_diary/src/App.jsx
+++ 12_emotion_diary/src/App.jsx
@@ -1,20 +1,33 @@
 import './App.css'
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
 
 function App() {
+  const nav = useNavigate();
 
+  const onClickButton = () => {
+    nav("/new")
+  }
+  
   return (
+    <>
+    <div>
+      <Link to ={"/"}>Home</Link>
+      <Link to ={"/new"}>New</Link>
+      <Link to ={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onClickButton}>새로운 페이지로 이동!</button>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/new' element={<New/>} />
       <Route path='/diary' element={<Diary/>} />
       <Route path='*' element={<Notfound/>} />
     </Routes>
+    </>
   )
 }
 
