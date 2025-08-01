09d913c150509a31b937b524b2da27a06ed3ef1d
Kangdy <canfrog@naver.com>
2025-08-01 11:05:42 +0900

    Dynamic Routing

--- 12_emotion_diary/src/pages/Home.jsx
+++ 12_emotion_diary/src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import { useSearchParams } from "react-router-dom"
+
 const Home = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("value"));
+
+  const updateQuery = () => {
+    setSearchParams({ value: '4', filter: 'active' });
+  };
+
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      
+      <p>현재 value: {searchParams.get('value')}</p>
+       <button onClick={updateQuery}>쿼리 변경</button>
+    </>
   )
 }
 
