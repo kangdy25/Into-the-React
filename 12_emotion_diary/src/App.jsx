59ad09ebc8cc29db3eb375857f24c72b17fcc198
Shineast <canfrog@naver.com>
2025-08-02 19:20:03 +0900

    Font, Image Setting

--- 12_emotion_diary/src/App.jsx
+++ 12_emotion_diary/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
+import { getEmotionImages } from './util/get-emotion-image'
 
 function App() {
   const nav = useNavigate();
@@ -15,6 +16,13 @@ function App() {
   
   return (
     <>
+    <div className="">
+      <img src={getEmotionImages(1)} alt="emotion" />
+      <img src={getEmotionImages(2)} alt="emotion" />
+      <img src={getEmotionImages(3)} alt="emotion" />
+      <img src={getEmotionImages(4)} alt="emotion" />
+      <img src={getEmotionImages(5)} alt="emotion" />
+    </div>
     <div>
       <Link to ={"/"}>Home</Link>
       <Link to ={"/new"}>New</Link>
