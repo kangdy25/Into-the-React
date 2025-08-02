a0ec22daba2ca8dbcb99fc36210fa73e92c1bf91
Shineast <canfrog@naver.com>
2025-08-02 19:52:28 +0900

    Implement Button Component

--- 12_emotion_diary/src/App.jsx
+++ 12_emotion_diary/src/App.jsx
@@ -6,35 +6,29 @@ import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
 import { getEmotionImages } from './util/get-emotion-image'
+import Button from './components/Button'
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new")
-  }
+  const nav = useNavigate();  
   
   return (
     <>
-    <div className="">
-      <img src={getEmotionImages(1)} alt="emotion" />
-      <img src={getEmotionImages(2)} alt="emotion" />
-      <img src={getEmotionImages(3)} alt="emotion" />
-      <img src={getEmotionImages(4)} alt="emotion" />
-      <img src={getEmotionImages(5)} alt="emotion" />
-    </div>
-    <div>
-      <Link to ={"/"}>Home</Link>
-      <Link to ={"/new"}>New</Link>
-      <Link to ={"/diary"}>Diary</Link>
-    </div>
-    <button onClick={onClickButton}>새로운 페이지로 이동!</button>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/new' element={<New/>} />
-      <Route path='/diary/:id' element={<Diary/>} />
-      <Route path='*' element={<Notfound/>} />
-    </Routes>
+      <Button text={'hello'} type={'POSITIVE'} onClick={()=>{
+        console.log('hello~~')
+      }}/>
+      <Button text={'hello'} type={'DEFAULT'} onClick={()=>{
+        console.log('hello~~')
+      }}/>
+      <Button text={'hello'} type={'NEGATIVE'} onClick={()=>{
+        console.log('hello~~')
+      }}/>
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/new' element={<New/>} />
+        <Route path='/diary/:id' element={<Diary/>} />
+        <Route path='*' element={<Notfound/>} />
+      </Routes>
     </>
   )
 }
--- 12_emotion_diary/src/pages/Home.jsx
+++ 12_emotion_diary/src/pages/Home.jsx
@@ -3,17 +3,10 @@ import { useSearchParams } from "react-router-dom"
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams.get("value"));
-
-  const updateQuery = () => {
-    setSearchParams({ value: '4', filter: 'active' });
-  };
-
+  
   return (
     <>
       <div>Home</div>
-      
-      <p>현재 value: {searchParams.get('value')}</p>
-       <button onClick={updateQuery}>쿼리 변경</button>
     </>
   )
 }
