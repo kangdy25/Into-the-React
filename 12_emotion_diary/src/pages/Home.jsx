7a3265644498bb8d0a46896cc227ecbe75ccb855
Kangdy <canfrog@naver.com>
2025-08-07 10:57:17 +0900

    Implement Change Date Function

--- 12_emotion_diary/src/pages/Home.jsx
+++ 12_emotion_diary/src/pages/Home.jsx
@@ -1,21 +1,27 @@
-import { useSearchParams } from "react-router-dom"
 import Header from '../components/Header';
 import Button from '../components/Button';
 import DiaryList from "../components/DiaryList";
+import { useState } from 'react';
 
 const Home = () => {
-  
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1))
+  }
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1))
+  }
+
   return (
-    <>
-      <div>
-        <Header 
-          title={"2024년 2월"}
-          leftChild={<Button text={"<"} />}
-          rightChild={<Button text={">"} />}
-        />
-        <DiaryList />
-      </div>
-    </>
+    <div>
+      <Header 
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+      />
+      <DiaryList />
+    </div>
   )
 }
 
