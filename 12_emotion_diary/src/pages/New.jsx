941a214ddeec712eef145e9ef9a43fb208e9bda0
Shineast <canfrog@naver.com>
2025-08-08 17:04:58 +0900

    Date Formatting in New Page

--- 12_emotion_diary/src/pages/New.jsx
+++ 12_emotion_diary/src/pages/New.jsx
@@ -1,13 +1,15 @@
 import Button from "../components/Button"
 import Header from "../components/Header"
 import Editor from "../components/Editor"
+import { useNavigate } from "react-router-dom"
 
 const New = () => {
+  const nav = useNavigate();
   return (
     <div>
       <Header
         title={"새 일기 쓰기"} 
-        leftChild={<Button text={"< 뒤로 가기"}/>} 
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"}/>} 
       />
       <Editor />
     </div>
