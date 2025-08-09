a8c32458db8c6d5ddc284a49d8cfc10936a986d2
Kangdy <canfrog@naver.com>
2025-08-09 22:27:38 +0900

    Implement Edit Page UI

--- 12_emotion_diary/src/pages/Edit.jsx
+++ 12_emotion_diary/src/pages/Edit.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom"
+import Header from "../components/Header"
+import Button from "../components/Button"
+import Editor from "../components/Editor"
 
 const Edit = () => {
   const params = useParams();
 
   return (
-    <div>{params.id}번 일기 수정되었어요~</div>
+    <div>
+      <Header 
+        title={"일기 수정하기"} 
+        leftChild={<Button text={"< 뒤로 가기"} />}
+        rightChild={<Button text={"삭제하기"} type={"NEGATIVE"} />}
+      />
+      <Editor />
+    </div>
   )
 }
 
