642a8feb0fb7f5fbc9b7bb926055fa8bf0fb5a64
Kangdy <canfrog@naver.com>
2025-08-09 22:35:39 +0900

    Implement Button Function in Edit Header

--- 12_emotion_diary/src/pages/Edit.jsx
+++ 12_emotion_diary/src/pages/Edit.jsx
@@ -1,17 +1,33 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import Header from "../components/Header"
 import Button from "../components/Button"
 import Editor from "../components/Editor"
+import { useContext } from "react"
+import { DiaryDispatchContext } from "../App"
 
 const Edit = () => {
   const params = useParams();
+  console.log(params)
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext)
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않습니다.")) {
+      onDelete(params.id);
+      nav('/', { replace : true });
+    }
+  }
 
   return (
     <div>
       <Header 
         title={"일기 수정하기"} 
-        leftChild={<Button text={"< 뒤로 가기"} />}
-        rightChild={<Button text={"삭제하기"} type={"NEGATIVE"} />}
+        leftChild={
+          <Button onClick={()=>nav(-1)} text={"< 뒤로 가기"} />
+        }
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
       />
       <Editor />
     </div>
