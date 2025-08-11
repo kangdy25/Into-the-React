f19f5e727487a514d4fa42d72e9350d4bec0ebd5
Kangdy <canfrog@naver.com>
2025-08-11 20:38:39 +0900

    Use Local Storage

--- 12_emotion_diary/src/App.jsx
+++ 12_emotion_diary/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useReducer, useRef, createContext } from 'react'
+import { useReducer, useRef, createContext, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import New from './pages/New'
@@ -7,50 +7,64 @@ import Diary from './pages/Diary'
 import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date("2025-08-07").getTime(),
-    emotionId: 1,
-    content: "1번 읽기 내용",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2025-08-06").getTime(),
-    emotionId: 2,
-    content: "2번 읽기 내용",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2025-07-21").getTime(),
-    emotionId: 3,
-    content: "3번 읽기 내용",
-  },
-]
-
 function reducer(state, action) {
+  let nextState;
+
   switch(action.type) {
-    case 'CREATE': 
-      return [action.data, ...state];
-    case 'UPDATE':
-      return state.map((item) => 
+    case 'INIT': 
+      return action.data;
+    case 'CREATE': {  
+      nextState = [action.data, ...state];
+      break;
+    };
+    case 'UPDATE': {
+      nextState = state.map((item) => 
         String(item.id) === String(action.data.id) ? action.data : item
-      )
-    case 'DELETE':
-      return state.filter(
-        (item) => String(item.id) !== String(action.data.id)
-      )
+      );
+      break;
+    }
+    case 'DELETE': {
+        nextState = state.filter(
+          (item) => String(item.id) !== String(action.data.id)
+        )
+        break;
+      } 
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (!storedData) return;
+
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) return;
+
+    let maxId = 0;
+    parsedData.forEach(item => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id)
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: "INIT",
+      data: parsedData
+    })
+  }, [])
 
   // 일기 추가 (C)
   const onCreate = (createdDate, emotionId, content) => {
